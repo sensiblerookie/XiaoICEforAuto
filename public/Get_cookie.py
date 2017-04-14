48837b4559f2ba9a94fd8a0e2201838b1254a8dc
sensiblerookie <fuyuan@outlook.com>
2017-04-14 19:29:23 +0800

    修改时间为7：30

--- public/Get_cookie.py
+++ public/Get_cookie.py
@@ -1,19 +1,54 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(30)
-base_url = "http://csint.msxiaobing.com/"
-driver.get(base_url)
+# driver = webdriver.Chrome()
+# driver.implicitly_wait(30)
+# base_url = "http://csint.msxiaobing.com/"
+# driver.get(base_url)
+# #
+# driver.find_element_by_id('phoneNumber').send_keys('15810189481')
+# driver.find_element_by_id('sendverification').click()
+# ip = input('请输入验证码：')
+# driver.find_element_by_id('verification').send_keys(ip)
+# driver.find_element_by_class_name('sbtn').click()
+# driver.find_element_by_class_name('guide_button').click()
+#
+# txt = open('cookie.txt', 'w+')
+# txt.write('[')
+# for cookie in driver.get_cookies():
+#     txt.writelines('{\'value\': \'%s\', \'name\': \' %s\'}, ' % (cookie['value'], cookie['name']))
+# txt.write('{\'value\': \'true\', \'name\': \'isQqQrCodeShown\'}]')
+# txt.close()
+#
+# f = open('cookie.txt', 'r')
+# # add_cookie = [{'value': '9ADE3954F3B4EA14E3C2ECF95CAA44A0', 'name': ' salt'}, {'value': '330668c806b75cdab4f68059491ca3d63e6e89ca6482b1be0fea83130f3d4e5b', 'name': ' ARRAffinity'}, {'value': 'dpz33x0qcqnymc1a35sgszo0', 'name': ' ASP.NET_SessionId'}, {'value': 'GDXesCFMxLRcNVNJfklUNTOyFEpiMF83WTRFMiU0LzRPAA', 'name': ' cpid'}, {'value': 'PvVAkRLnvfiAG2YpL0ZPplnqFKF3BaLP4cmTKQDv7MwzrfC1TbXCbbNwOWZj_Y68JuOjTWSTenz86WwkTzlj4ZKEWMv3ErutS0goXP5WkNU1', 'name': ' __RequestVerificationToken'}, {'value': '1', 'name': ' _gat'}, {'value': 'GA1.2.753731095.1492159007', 'name': ' _ga'}, {'value': '725A884969598F6BCD79C54A79913FB10E4423C1C1736B7722BB51209D643074C923155BA4E166A4EAA14FF363C644C393BFF86F1495681B26115E59242A8699DA7A1A0BAC1674C415B77B890CAAFCE6', 'name': ' .ASPXAUTH'}, {'value': 'EulaAcceptance201703', 'name': ' ForceSignOut'}, {'value': 'true', 'name': ' isQqQrCodeShown'}, {'value': '30', 'name': ' hideFaqGuideInHome'}, {'value': 'true', 'name': 'isQqQrCodeShown'}]
+# add_cookie = [{'value': '30', 'name': 'hideFaqGuideInHome'},
+#               {'value': 'D32D51A9DE047D4622A38E4F292B5B84', 'name': 'salt'},
+#               {'value': '330668c806b75cdab4f68059491ca3d63e6e89ca6482b1be0fea83130f3d4e5b', 'name': 'ARRAffinity'},
+#               {'value': 'ruu1zpdmenm5leujodag1umc', 'name': 'ASP.NET_SessionId'},
+#               {'value': 'GEsnTlhMQUoiTtE0eklXTcw0F0tgSSVJJzRCsFg2KUtOAA', 'name': 'cpid'},
+#               {'value': 'i-HBnctXeq9GX2NR9zbw4UgWs-BYY2ZQf0qSTDYXIrmrrDY8iOCwqBB5lXRPulXvDHHw3T-TBY7E310FIAERFl3WFHdMTdebt9QGyvnwZLk1', 'name': '__RequestVerificationToken'},
+#               {'value': 'GA1.2.32620240.1492159845', 'name': '_ga'},
+#               {'value': 'E1C59EB04299B25B5E673298B5E2F2CB5CF297C6F64984F7DF2D15057CDAADAA23A778D1814BAE6B6E555AC4C48F13305CC2F6195F08D9E350EBA2A3BD98F084547B8A670B099B18C6189E6DDEB7DCF5', 'name': '.ASPXAUTH'},
+#               {'value': 'EulaAcceptance201703', 'name': 'ForceSignOut'},
+#               {'value': 'true', 'name': 'isQqQrCodeShown'}]
+# for i in range(0, len(add_cookie)):
+#     driver.add_cookie(add_cookie[i])
+#
+# driver.get(base_url)
+#
+# print(len(add_cookie))
 
-driver.find_element_by_id('phoneNumber').send_keys('15810189481')
-driver.find_element_by_id('sendverification').click()
-ip = input('请输入验证码：')
-driver.find_element_by_id('verification').send_keys(ip)
-driver.find_element_by_class_name('sbtn').click()
 
-cookie = driver.get_cookies()
+f = open('cookie.txt', 'r')
+fc = f.read()
+print(fc)
+# a = ['%s' % fc]
+#
+# print(len(a))
+# print(a)
 
-print('{\'name\': \'%s\', \'value\':\' %s\'}' % (cookie['name'], cookie['value']))
-
-
+x = [1]
+y = {1, '\'j\': \'1\''}
+x.append(y)
+print(x)
--- test_case/test_FAQ_for_home.py
+++ test_case/test_FAQ_for_home.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-@others:     DTStudio, All rights reserved-- Created on 2017/03/29
-@desc:       使用unittest组织用例
-"""
+
 from selenium import webdriver
 import unittest
 import time
@@ -17,11 +13,10 @@ class TestSearch(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.base_url = "http://csint.msxiaobing.com/"
         self.driver.get(self.base_url)
-        #   int环境，15810189481的手机号cookie
-        add_cookie = [{'value': 'true', 'name': 'isQqQrCodeShown'}, {'value': 'E913C694C82D08FAA7C8C3F632644D2C', 'name': 'salt'}, {'value': 'd6615295f92e35a4fc39e36bff91c1f8f015b9a1729023858a4ef3c8bad31c98', 'name': 'ARRAffinity'}, {'value': '3y2bsmcm11cx5uh1tidz1q3j', 'name': 'ASP.NET_SessionId'}, {'value': 'YDVasiE2QkhaNFFJeUkuTMtMEEpnMd4wWrFEN1pPL0xNAA', 'name': 'cpid'}, {'value': 'xdge3nE9mGhqhYm0nb8doTqSoufM6RUiuU-b7v4Oz4aCxC79nBkGlOAalBwc5ILjj0t0HaANd3wrsdyXUUEPsq0MXZ9WcaAx7mM-3lRYKLU1', 'name': '__RequestVerificationToken'}, {'value': '1', 'name': '_gat'}, {'value': 'GA1.2.2047437516.1489842596', 'name': '_ga'}, {'value': '9FB5B8CA00779E60FC40814D6359DF4EB5DB6E35CFD91A122F6F208E2594C2E8AFCE37B0AD392232F47AEAE6E3F68D27F626B3BEDBCD77312FDD456C528600C62A6E28AF013571E8EFA69740B7178CB3', 'name': '.ASPXAUTH'}, {'value': 'EulaAcceptance201703', 'name': 'ForceSignOut'}, {'value': 'true', 'name': 'isQqQrCodeShown'}]
-        #   prod环境，13718161821的手机号cookie
-        # add_cookie = [{'value': 'fcfe77ea6824afb8624e05c93de1dd5932aaa6cbb5c35316ae3c0aa618edf44d', 'name': 'ARRAffinity'}, {'value': 'YE0lNllLRDJeTlRNeDFUMzO0lDBgSSVJJk1BN11LVrJPAA', 'name': 'cpid'}, {'value': 'opmgxjw0lpyawp2t4yldwoxq', 'name': 'ASP.NET_SessionId'}, {'value': 'uPELacUPOQ43D8t9Ymta2zpdtGzGHGMqxXBYTImp0CK1ewd-C-Ae9BkT3HDexhCkXtZs-pkeRzOwMIAy0RsQ1j19gLcSIOvy-G8SetkBLMg1', 'name': '__RequestVerificationToken'}, {'value': 'GA1.2.2117121162.1490166469', 'name': '_ga'}, {'value': '1', 'name': '_gat'}, {'value': '67B514E7BDD4F4309890731099D101877D657BF1874E8D3F0542D6939CCBC9A7C2F8FF09E519366DEF3994039BF692396058FB6637CAA95D02F7B02F9C435DDD0FCC7DE2538E8122676A9482DE3AFD4A', 'name': '.ASPXAUTH'}, {'value': 'EulaAcceptance201703', 'name': 'ForceSignOut'}, {'value': '736C7959BCC6EA7736BA7B6E6511A0F6', 'name': 'salt'}, {'value': 'true', 'name': 'isQqQrCodeShown'}]
-        for i in range(0, len(add_cookie)):
+
+
+        # add_cookie = [{'value': 'true', 'name': 'isQqQrCodeShown'}, {'value': 'E913C694C82D08FAA7C8C3F632644D2C', 'name': 'salt'}, {'value': 'd6615295f92e35a4fc39e36bff91c1f8f015b9a1729023858a4ef3c8bad31c98', 'name': 'ARRAffinity'}, {'value': '3y2bsmcm11cx5uh1tidz1q3j', 'name': 'ASP.NET_SessionId'}, {'value': 'YDVasiE2QkhaNFFJeUkuTMtMEEpnMd4wWrFEN1pPL0xNAA', 'name': 'cpid'}, {'value': 'xdge3nE9mGhqhYm0nb8doTqSoufM6RUiuU-b7v4Oz4aCxC79nBkGlOAalBwc5ILjj0t0HaANd3wrsdyXUUEPsq0MXZ9WcaAx7mM-3lRYKLU1', 'name': '__RequestVerificationToken'}, {'value': '1', 'name': '_gat'}, {'value': 'GA1.2.2047437516.1489842596', 'name': '_ga'}, {'value': '9FB5B8CA00779E60FC40814D6359DF4EB5DB6E35CFD91A122F6F208E2594C2E8AFCE37B0AD392232F47AEAE6E3F68D27F626B3BEDBCD77312FDD456C528600C62A6E28AF013571E8EFA69740B7178CB3', 'name': '.ASPXAUTH'}, {'value': 'EulaAcceptance201703', 'name': 'ForceSignOut'}, {'value': 'true', 'name': 'isQqQrCodeShown'}]
+        # for i in range(0, len(add_cookie)):
             self.driver.add_cookie(add_cookie[i])
 
     def test_FAQ_home(self):
